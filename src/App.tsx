97a2f1d45660357688a622e7a4bd48734397e5ed
tgoldfrad <tgoldfrad8822@gmail.com>
2025-02-01 21:31:40 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import './App.css'
+
 import Start from './components/user/Start'
 
 
@@ -8,15 +8,9 @@ function App() {
 
   return (
     <>
-      <div style={{
-        height: "calc(100vh - 18px)",
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        backgroundImage: "linear-gradient(rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6)), url(src/assets/recipe.jpg)"
-      }}>
+ 
         <Start />
-      </div>
+    
 
     </>
   )
