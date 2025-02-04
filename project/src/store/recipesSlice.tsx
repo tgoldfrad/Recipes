caeea4dc604639eef573089e51cc630a5f838807
tgoldfrad <tgoldfrad8822@gmail.com>
2025-02-04 16:30:06 +0200

    final-final

--- project/src/store/recipesSlice.tsx
+++ project/src/store/recipesSlice.tsx
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { RecipeType } from "../types";
 import axios from "axios";
-//לבדוק האם להוסיף עוד addcase
+
 export const fetchRecipes = createAsyncThunk('recipes/fetch',async (_, thunkAPI)=>{
     try{
         const response = await axios.get('http://localhost:3000/api/recipes');
@@ -25,9 +25,8 @@ const recipesSlice = createSlice({
     name: 'recipes',
     initialState: {list:[] as RecipeType[],loading:false,error:null as string|null},
     reducers: {
-        
     },
-    //PayLoadAction<recipeType>
+ 
     extraReducers:(builder)=>{
         builder
         .addCase(fetchRecipes.pending,(state)=>{
@@ -35,27 +34,22 @@ const recipesSlice = createSlice({
             state.error = null;
         })
        .addCase(fetchRecipes.fulfilled, (state, action) => {
-           // alert("success")
            state.loading = false;
             state.list = [...action.payload]
         })
         .addCase(fetchRecipes.rejected, (state, action) => {
             state.loading = false;
             state.error = action.payload as string;
-            //alert("error: sumthing is not correct");
         })
         .addCase(addRecipe.pending,(state)=>{
             state.loading = true;
             state.error = null;
         })
         .addCase(addRecipe.fulfilled, (state, action) => {
-           // alert("success")
-            //state.list = [...state.list, {...action.payload}]
             state.loading = false;
             state.list.push(action.payload)
         })
         .addCase(addRecipe.rejected, (state, action) => {
-            //alert("error: sumthing is not correct");
             state.loading = false;
             state.error = action.payload as string;
         })
